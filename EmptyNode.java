7fb81e24acfa404c8fee7f56689a84302cf26e66
Anurag Pokala <93160263+anuragpokala@users.noreply.github.com>
2025-12-08 23:00:31 -0500

    Create EmptyNode.java

---
+++ EmptyNode.java
@@ -0,0 +1,54 @@
+/**
+ * Flyweight Empty Node.
+ *
+ * @author {Your Name Here}
+ * @version {Put Something Here}
+ */
+public class EmptyNode implements BinNode {
+    
+    public static final EmptyNode EMPTY = new EmptyNode();
+
+    private EmptyNode() {}
+
+    @Override
+    public BinNode insert(AirObject a, int x, int y, int z, int w, int h, int d, int level) {
+        LeafNode node = new LeafNode();
+        return node.insert(a, x, y, z, w, h, d, level);
+    }
+
+    @Override
+    public BinNode delete(AirObject a, int x, int y, int z, int w, int h, int d, int level) {
+        return this;
+    }
+
+    @Override
+    public int count() {
+        return 0;
+    }
+
+    @Override
+    public int countNodes() {
+        return 1;
+    }
+
+    @Override
+    public boolean isLeaf() {
+        return true;
+    }
+
+    @Override
+    public String print(int x, int y, int z, int w, int h, int d, int level) {
+        return "E (" + x + ", " + y + ", " + z + ", " + w + ", " + h + ", " + d + ") " + count() + "\n";
+    }
+
+    @Override
+    public int intersect(int x, int y, int z, int w, int h, int d, int level, 
+                         int qx, int qy, int qz, int qw, int qh, int qd, StringBuilder sb) {
+        return 1;
+    }
+
+    @Override
+    public void collisions(int x, int y, int z, int w, int h, int d, int level, StringBuilder sb) {
+        // No collisions in empty node
+    }
+}
