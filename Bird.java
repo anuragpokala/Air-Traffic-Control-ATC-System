684469b92cc47a575fe885a0f95bf10521f84a24
Anurag Pokala <93160263+anuragpokala@users.noreply.github.com>
2025-12-09 21:03:26 -0500

    Update Bird.java

--- Bird.java
+++ Bird.java
@@ -1,8 +1,9 @@
 /**
  * Bird object with type and count.
  *
- * @author {Your Name Here}
- * @version {Put Something Here}
+ * @author Anurag Pokala (anuragp34) 
+ * @author Parth Mehta (pmehta24)
+ * @version 12/3/2025
  */
 public class Bird extends AirObject {
 
