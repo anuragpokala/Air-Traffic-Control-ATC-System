1dca5ded929651fb70094eb5188fd4a2a84a1bf2
Anurag Pokala <93160263+anuragpokala@users.noreply.github.com>
2025-12-09 21:04:19 -0500

    Update InternalNode.java

--- InternalNode.java
+++ InternalNode.java
@@ -1,214 +1,197 @@
 /**
- * Internal Node implementation.
- *
- * @author {Your Name Here}
- * @version {Put Something Here}
+ * Internal Node of the Bintree.
+ * 
+ * @author Anurag Pokala (anuragp34) 
+ * @author Parth Mehta (pmehta24)
+ * @version 12/3/2025
  */
 public class InternalNode implements BinNode {
-    
+
     private BinNode left;
     private BinNode right;
 
+    /**
+     * Create new Internal Node with empty children.
+     */
     public InternalNode() {
-        left = EmptyNode.EMPTY;
-        right = EmptyNode.EMPTY;
+        left = EmptyNode.THE_NODE;
+        right = EmptyNode.THE_NODE;
+    }
+
+    private boolean overlaps(int s1, int e1, int s2, int e2) {
+        return s1 < e2 && s2 < e1;
+    }
+
+    private boolean boxIntersectsObj(SpatialBox box, AirObject obj) {
+        return overlaps(box.getX(), box.getX() + box.getXDimension(), obj.getXorig(), obj.getXorig() + obj.getXwidth()) &&
+               overlaps(box.getY(), box.getY() + box.getYDimension(), obj.getYorig(), obj.getYorig() + obj.getYwidth()) &&
+               overlaps(box.getZ(), box.getZ() + box.getZDimension(), obj.getZorig(), obj.getZorig() + obj.getZwidth());
     }
 
     @Override
-    public BinNode insert(AirObject a, int x, int y, int z, int w, int h, int d, int level) {
-        int dim = level % 3;
-        int mid;
-        
-        if (dim == 0) { // x split
-            mid = x + w / 2;
-            if (a.getXorig() < mid) {
-                left = left.insert(a, x, y, z, w / 2, h, d, level + 1);
-            } else {
-                right = right.insert(a, mid, y, z, w / 2, h, d, level + 1);
-            }
-        } else if (dim == 1) { // y split
-            mid = y + h / 2;
-            if (a.getYorig() < mid) {
-                left = left.insert(a, x, y, z, w, h / 2, d, level + 1);
-            } else {
-                right = right.insert(a, x, mid, z, w, h / 2, d, level + 1);
-            }
-        } else { // z split
-            mid = z + d / 2;
-            if (a.getZorig() < mid) {
-                left = left.insert(a, x, y, z, w, h, d / 2, level + 1);
-            } else {
-                right = right.insert(a, x, y, mid, w, h, d / 2, level + 1);
-            }
+    public BinNode insert(AirObject obj, SpatialBox box, int level) {
+        int axis = level % 3;
+        SpatialBox b1 = box.getFirstHalf(axis);
+        SpatialBox b2 = box.getSecondHalf(axis);
+
+        if (boxIntersectsObj(b1, obj)) {
+            left = left.insert(obj, b1, level + 1);
         }
-        
+        if (boxIntersectsObj(b2, obj)) {
+            right = right.insert(obj, b2, level + 1);
+        }
         return this;
     }
 
     @Override
-    public BinNode delete(AirObject a, int x, int y, int z, int w, int h, int d, int level) {
-        int dim = level % 3;
-        int mid;
-        
-        if (dim == 0) { // x
-            mid = x + w / 2;
-            if (a.getXorig() < mid) {
-                left = left.delete(a, x, y, z, w / 2, h, d, level + 1);
-            } else {
-                right = right.delete(a, mid, y, z, w / 2, h, d, level + 1);
-            }
-        } else if (dim == 1) { // y
-            mid = y + h / 2;
-            if (a.getYorig() < mid) {
-                left = left.delete(a, x, y, z, w, h / 2, d, level + 1);
-            } else {
-                right = right.delete(a, x, mid, z, w, h / 2, d, level + 1);
-            }
-        } else { // z
-            mid = z + d / 2;
-            if (a.getZorig() < mid) {
-                left = left.delete(a, x, y, z, w, h, d / 2, level + 1);
-            } else {
-                right = right.delete(a, x, y, mid, w, h, d / 2, level + 1);
-            }
+    public BinNode remove(String name, SpatialBox box, RemovalContainer held, int level) {
+        int axis = level % 3;
+        SpatialBox b1 = box.getFirstHalf(axis);
+        SpatialBox b2 = box.getSecondHalf(axis);
+
+        left = left.remove(name, b1, held, level + 1);
+        right = right.remove(name, b2, held, level + 1);
+
+
+
+        if (shouldMerge()) {
+            return performMerge();
         }
-        
-        // Merge check
-        if (left.isLeaf() && right.isLeaf()) {
-            int total = left.count() + right.count();
-            if (total == 0) {
-                return EmptyNode.EMPTY;
-            }
-            if (total == 1) {
-                LeafNode newLeaf = new LeafNode();
-                mergeInto(newLeaf, left);
-                mergeInto(newLeaf, right);
-                return newLeaf;
-            }
-        }
-        
         return this;
     }
-    
-    private void mergeInto(LeafNode target, BinNode source) {
-        if (source instanceof LeafNode) {
-            ((LeafNode) source).transferTo(target);
+
+    private boolean isLeafOrEmpty(BinNode node) {
+        return node == EmptyNode.THE_NODE || node instanceof LeafNode;
+    }
+
+    private boolean shouldMerge() {
+        if (!isLeafOrEmpty(left) || !isLeafOrEmpty(right)) return false;
+
+        AirObject[] all = getAllUnique();
+        int count = 0;
+        for (AirObject a : all) {
+            if (a != null) count++;
         }
+
+        if (count <= 3) return true;
+        
+
+        if (count == 0) return true;
+        int minX = all[0].getXorig();
+        int maxX = minX + all[0].getXwidth();
+        int minY = all[0].getYorig();
+        int maxY = minY + all[0].getYwidth();
+        int minZ = all[0].getZorig();
+        int maxZ = minZ + all[0].getZwidth();
+
+        for (int i = 1; i < count; i++) {
+            AirObject obj = all[i];
+            minX = Math.max(minX, obj.getXorig());
+            maxX = Math.min(maxX, obj.getXorig() + obj.getXwidth());
+            minY = Math.max(minY, obj.getYorig());
+            maxY = Math.min(maxY, obj.getYorig() + obj.getYwidth());
+            minZ = Math.max(minZ, obj.getZorig());
+            maxZ = Math.min(maxZ, obj.getZorig() + obj.getZwidth());
+
+            if (minX >= maxX || minY >= maxY || minZ >= maxZ) {
+                return false;
+            }
+        }
+        return true;
     }
 
-    @Override
-    public int count() {
-        return left.count() + right.count();
+    private AirObject[] getAllUnique() {
+        int cap = 0;
+        if (left instanceof LeafNode) cap += ((LeafNode) left).getItemCount();
+        if (right instanceof LeafNode) cap += ((LeafNode) right).getItemCount();
+        
+        AirObject[] arr = new AirObject[cap];
+        int idx = 0;
+
+        if (left instanceof LeafNode) {
+            LeafNode l = (LeafNode) left;
+            for (int i = 0; i < l.getItemCount(); i++) {
+                if (!contains(arr, idx, l.getArray()[i].getName())) {
+                    arr[idx++] = l.getArray()[i];
+                }
+            }
+        }
+        if (right instanceof LeafNode) {
+            LeafNode r = (LeafNode) right;
+            for (int i = 0; i < r.getItemCount(); i++) {
+                if (!contains(arr, idx, r.getArray()[i].getName())) {
+                    arr[idx++] = r.getArray()[i];
+                }
+            }
+        }
+        
+
+        AirObject[] finalArr = new AirObject[idx];
+        System.arraycopy(arr, 0, finalArr, 0, idx);
+        return finalArr;
     }
 
-    @Override
-    public int countNodes() {
-        return 1 + left.countNodes() + right.countNodes();
-    }
-
-    @Override
-    public boolean isLeaf() {
+    private boolean contains(AirObject[] arr, int len, String name) {
+        for (int i = 0; i < len; i++) {
+            if (arr[i].getName().equals(name)) {
+                return true;
+            }
+        }
         return false;
     }
 
-    @Override
-    public String print(int x, int y, int z, int w, int h, int d, int level) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < level; i++) {
-            sb.append("  ");
-        }
-        sb.append("I (").append(x).append(", ").append(y).append(", ").append(z)
-          .append(", ").append(w).append(", ").append(h).append(", ").append(d)
-          .append(") ").append(count()).append("\n");
+    private BinNode performMerge() {
+        AirObject[] objs = getAllUnique();
+        if (objs.length == 0) return EmptyNode.THE_NODE;
         
-        int dim = level % 3;
-        // Left
-        sb.append(left.print(getChildX(0, dim, x, w), getChildY(0, dim, y, h), getChildZ(0, dim, z, d),
-                             getChildW(0, dim, w),    getChildH(0, dim, h),    getChildD(0, dim, d),
-                             level + 1));
-        // Right
-        sb.append(right.print(getChildX(1, dim, x, w), getChildY(1, dim, y, h), getChildZ(1, dim, z, d),
-                              getChildW(1, dim, w),    getChildH(1, dim, h),    getChildD(1, dim, d),
-                              level + 1));
-        return sb.toString();
+        LeafNode newLeaf = new LeafNode(objs[0]);
+        for (int i = 1; i < objs.length; i++) {
+            newLeaf.addDirect(objs[i]);
+        }
+        return newLeaf;
     }
 
     @Override
-    public int intersect(int x, int y, int z, int w, int h, int d, int level,
-                          int qx, int qy, int qz, int qw, int qh, int qd, StringBuilder sb) {
-        int visited = 1; // Visit self
-        int dim = level % 3;
+    public void print(StringBuilder sb, SpatialBox box, int level, IntWrapper count) {
+        StringBuilder space = new StringBuilder();
+        for (int i = 0; i < level * 2; i++) space.append(" ");
         
-        // Left child
-        int lx = getChildX(0, dim, x, w);
-        int ly = getChildY(0, dim, y, h);
-        int lz = getChildZ(0, dim, z, d);
-        int lw = getChildW(0, dim, w);
-        int lh = getChildH(0, dim, h);
-        int ld = getChildD(0, dim, d);
+        sb.append(space).append("I ").append(box.toString()).append(" ")
+          .append(level).append("\r\n");
+        count.increment();
         
-        if (boxIntersect(lx, ly, lz, lw, lh, ld, qx, qy, qz, qw, qh, qd)) {
-            visited += left.intersect(lx, ly, lz, lw, lh, ld, level + 1, qx, qy, qz, qw, qh, qd, sb);
-        }
-        
-        // Right child
-        int rx = getChildX(1, dim, x, w);
-        int ry = getChildY(1, dim, y, h);
-        int rz = getChildZ(1, dim, z, d);
-        int rw = getChildW(1, dim, w);
-        int rh = getChildH(1, dim, h);
-        int rd = getChildD(1, dim, d);
-        
-        if (boxIntersect(rx, ry, rz, rw, rh, rd, qx, qy, qz, qw, qh, qd)) {
-            visited += right.intersect(rx, ry, rz, rw, rh, rd, level + 1, qx, qy, qz, qw, qh, qd, sb);
-        }
-        
-        return visited;
+        int axis = level % 3;
+        left.print(sb, box.getFirstHalf(axis), level + 1, count);
+        right.print(sb, box.getSecondHalf(axis), level + 1, count);
     }
 
     @Override
-    public void collisions(int x, int y, int z, int w, int h, int d, int level, StringBuilder sb) {
-        int dim = level % 3;
-        // Left
-        left.collisions(getChildX(0, dim, x, w), getChildY(0, dim, y, h), getChildZ(0, dim, z, d),
-                        getChildW(0, dim, w),    getChildH(0, dim, h),    getChildD(0, dim, d),
-                        level + 1, sb);
-        // Right
-        right.collisions(getChildX(1, dim, x, w), getChildY(1, dim, y, h), getChildZ(1, dim, z, d),
-                         getChildW(1, dim, w),    getChildH(1, dim, h),    getChildD(1, dim, d),
-                         level + 1, sb);
+    public void findCollisions(StringBuilder sb, SpatialBox box, int level) {
+        int axis = level % 3;
+        left.findCollisions(sb, box.getFirstHalf(axis), level + 1);
+        right.findCollisions(sb, box.getSecondHalf(axis), level + 1);
     }
 
-    private boolean boxIntersect(int x1, int y1, int z1, int w1, int h1, int d1,
-                                 int x2, int y2, int z2, int w2, int h2, int d2) {
-        return (x1 < x2 + w2 && x1 + w1 > x2 &&
-                y1 < y2 + h2 && y1 + h1 > y2 &&
-                z1 < z2 + d2 && z1 + d1 > z2);
+    @Override
+    public void findIntersections(SpatialBox query, SpatialBox box, StringBuilder sb, IntWrapper visits, int level) {
+        visits.increment();
+        sb.append("In Internal node ").append(box.toString()).append(" ").append(level).append("\r\n");
+        
+        int axis = level % 3;
+        SpatialBox b1 = box.getFirstHalf(axis);
+        SpatialBox b2 = box.getSecondHalf(axis);
+
+        if (boxesOverlap(query, b1)) {
+            left.findIntersections(query, b1, sb, visits, level + 1);
+        }
+        if (boxesOverlap(query, b2)) {
+            right.findIntersections(query, b2, sb, visits, level + 1);
+        }
     }
 
-    // Helpers
-    private int getChildX(int side, int dim, int x, int w) {
-        if (dim == 0 && side == 1) return x + w / 2;
-        return x;
-    }
-    private int getChildY(int side, int dim, int y, int h) {
-        if (dim == 1 && side == 1) return y + h / 2;
-        return y;
-    }
-    private int getChildZ(int side, int dim, int z, int d) {
-        if (dim == 2 && side == 1) return z + d / 2;
-        return z;
-    }
-    private int getChildW(int side, int dim, int w) {
-        if (dim == 0) return w / 2;
-        return w;
-    }
-    private int getChildH(int side, int dim, int h) {
-        if (dim == 1) return h / 2;
-        return h;
-    }
-    private int getChildD(int side, int dim, int d) {
-        if (dim == 2) return d / 2;
-        return d;
+    private boolean boxesOverlap(SpatialBox a, SpatialBox b) {
+        return overlaps(a.getX(), a.getX() + a.getXDimension(), b.getX(), b.getX() + b.getXDimension()) &&
+               overlaps(a.getY(), a.getY() + a.getYDimension(), b.getY(), b.getY() + b.getYDimension()) &&
+               overlaps(a.getZ(), a.getZ() + a.getZDimension(), b.getZ(), b.getZ() + b.getZDimension());
     }
 }
